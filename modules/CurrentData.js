d6f5910f51c514776725112bb4b76ff90daf13cb
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-05-04 17:34:56 +0400

    Show current time dynamically

--- modules/CurrentData.js
+++ modules/CurrentData.js
@@ -3,16 +3,21 @@ import { DateTime } from '../node_modules/luxon/src/luxon.js';
 export default () => {
   document.addEventListener('DOMContentLoaded', () => {
     const time = document.querySelector('.time');
-    const now = DateTime.now();
-    const dateTime = now.toLocaleString({
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-      hour12: true,
-    });
-    time.textContent = dateTime;
+
+    const updateTime = () => {
+      const now = DateTime.now();
+      const dateTime = now.toLocaleString({
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true,
+      });
+      time.textContent = dateTime;
+    };
+    updateTime();
+    setInterval(updateTime, 1000);
   });
 };
