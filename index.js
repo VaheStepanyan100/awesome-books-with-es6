10d42209496405f53dfa98f0f6612a379ba748f1
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-05-04 17:07:17 +0400

    Display the current date below the navigation bar

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import InitLocalStorage from './modules/InitLocalStorage.js';
 import BooksCollection from './modules/BooksCollection.js';
 import NavBarEventListeners from './modules/NavBarEventListeners.js';
-
+import updateTime from './modules/CurrentData.js';
 const booksCollection = new BooksCollection();
 
 InitLocalStorage();
 booksCollection.init();
 booksCollection.displayBooks();
 NavBarEventListeners();
+updateTime();
